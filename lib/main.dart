1c6aa7df83ac73b94aed85011d2d660ff5a56a74
Maulana Yusuf <ymaulana089@gmail.com>
2021-11-06 11:18:46 +0700

    update http

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:conews/provider/news_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
@@ -6,12 +8,22 @@ import 'model/all_news_data.dart';
 import 'package:path_provider/path_provider.dart' as pathProvider;
 import 'ui/pages/splash_page.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   var appDocumentDirectory =
       await pathProvider.getApplicationDocumentsDirectory();
   Hive.init(appDocumentDirectory.path);
   Hive.registerAdapter(AllNewsDataAdapter());
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const MyApp());
 }
 
